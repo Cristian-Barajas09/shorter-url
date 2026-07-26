edf2e6872b48ffac176217a4f642fa1eb1af6d6b
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-07-25 22:09:19 -0400

    feat(settings): add alowed domains in settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ from config.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", '127.0.0.1']
+ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", cast=list)
 
 
 url: str = config('DATABASE_URL', cast=str) # type: ignore
