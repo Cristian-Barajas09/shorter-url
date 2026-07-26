9b90a9f41c49086d620fc8be2208d5e5c14fc8a2
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-07-25 22:22:52 -0400

    refactor(asgi, wsgi): simplify environment variable handling

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,11 +8,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from decouple import config as env_config
 
 from django.core.wsgi import get_wsgi_application
 
-environment = env_config("DJANGO_ENV", default="production", cast=str)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'config.settings.{environment}')
+environment = os.environ.get("DJANGO_ENV", "production")
+os.environ["DJANGO_SETTINGS_MODULE"] = f'config.settings.{environment}'
 
 application = get_wsgi_application()
