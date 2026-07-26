17b9bd96e31efd7dbb473740891a8bc53297c32d
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-07-25 22:33:21 -0400

    fix(settings): update ALLOWED_HOSTS configuration to use Csv for better handling

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,22 +1,27 @@
 """production settings"""
+
 import dj_database_url
-from decouple import config
+from decouple import config, Csv
 
 # pylint: disable=wildcard-import
 # pylint: disable=unused-wildcard-import
 from config.settings.base import *
+
 # pylint: enable=wildcard-import
 # pylint: enable=unused-wildcard-import
 
 DEBUG = False
 
-ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", cast=list)
+
+ALLOWED_HOSTS = config(
+    "DJANGO_ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv()
+)
 
 
-url: str = config('DATABASE_URL', cast=str) # type: ignore
+url: str = config("DATABASE_URL", cast=str)  # type: ignore
 
 DATABASES = {
-    'default': dj_database_url.config(
+    "default": dj_database_url.config(
         default=url,
         conn_max_age=600,
         conn_health_checks=True,
